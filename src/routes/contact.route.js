7bbb1bbec3e1d87b9ca4c693459d2a820ca4972b
MasonS <amsadeghi79@gmail.com>
2025-10-06 20:09:28 +0330

    refactor

--- src/routes/contact.route.js
+++ src/routes/contact.route.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/contact.controller");
-const {
-  validateContact,
-  validateContactUpdate,
-} = require("../utils/validators");
+const { validateContact } = require("../utils/validators");
 
 router.post("/", validateContact, controller.createContact);
 router.get("/", controller.getContacts);
 router.get("/search", controller.searchContacts);
 router.get("/:id", controller.getContactById);
-router.put("/:id", validateContactUpdate, controller.updateContact);
+router.put("/:id", validateContact, controller.updateContact);
 router.delete("/:id", controller.deleteContact);
 
 module.exports = router;
