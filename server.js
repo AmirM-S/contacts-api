6624f69b9bf73b3eb9c5d1661076189aa79d694d
MasonS <amsadeghi79@gmail.com>
2025-10-06 23:16:52 +0330

    update server

--- server.js
+++ server.js
@@ -6,13 +6,7 @@ const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-// برای Vercel
-if (process.env.NODE_ENV !== 'production') {
-  const server = app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
-  });
-}
-
-// Export برای Vercel
-module.exports = app;
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Environment: ${process.env.NODE_ENV || "development"}`);
+});
